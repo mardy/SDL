b3af786127d977236f1bd4146a8ffc8a83198d86
Alberto Mardegan <mardy@users.sourceforge.net>
2026-08-14 18:12:51 +0300

    ogc: add audio backend based on libaesnd

--- src/audio/SDL_audio.c
+++ src/audio/SDL_audio.c
@@ -80,6 +80,9 @@ static const AudioBootStrap *const bootstrap[] = {
 #ifdef SDL_AUDIO_DRIVER_NGAGE
     &NGAGEAUDIO_bootstrap,
 #endif
+#ifdef SDL_AUDIO_DRIVER_OGC
+    &OGCAUDIO_bootstrap,
+#endif
 #ifdef SDL_AUDIO_DRIVER_EMSCRIPTEN
     &EMSCRIPTENAUDIO_bootstrap,
 #endif
